c0ee23477e96f838536efd8b51275c8227b14213
Lars Sandberg <larslsa@stud.ntnu.no>
2019-01-09 21:45:18 +0100

    Create foo.go

---
+++ ex1/part4/go/foo.go
@@ -0,0 +1,35 @@
+// Use `go run foo.go` to run your program
+
+package main
+
+import (
+    . "fmt"
+    "runtime"
+    "time"
+)
+
+var i = 0
+
+func incrementing() {
+    for j := 0; j < 100000; j++ {
+		    i++
+	  } 
+}
+
+func decrementing() {
+    for j := 0; j < 100000; j++ {
+		    i++
+    } 
+}
+
+func main() {
+    runtime.GOMAXPROCS(runtime.NumCPU())    // I guess this is a hint to what GOMAXPROCS does...
+	                                    // Try doing the exercise both with and without it!
+    go incrementing()
+    go decrementing()
+	
+    // We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
+    // We'll come back to using channels in Exercise 2. For now: Sleep.
+    time.Sleep(100*time.Millisecond)
+    Println("The magic number is:", i)
+}
