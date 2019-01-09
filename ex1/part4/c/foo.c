50722e0b90ca71315ce7e41b002eca4cd303cef8
Lars Sandberg <larslsa@stud.ntnu.no>
2019-01-09 19:11:21 +0100

    Create foo.c

---
+++ ex1/part4/c/foo.c
@@ -0,0 +1,28 @@
+#include <pthread.h>
+#include <stdio.h>
+
+int i = 0;
+
+// Note the return type: void*
+void* incrementingThreadFunction(){
+    // TODO: increment i 1_000_000 times
+    return NULL;
+}
+
+void* decrementingThreadFunction(){
+    // TODO: decrement i 1_000_000 times
+    return NULL;
+}
+
+
+int main(){
+    // TODO: declare incrementingThread and decrementingThread (hint: google pthread_create)
+    pthread_create(&incrementingThread, NULL, incrementingThreadFunction, NULL);
+    pthread_create(&decrementingThread, NULL, decrementingThreadFunction, NULL);
+    
+    pthread_join(incrementingThread, NULL);
+    pthread_join(decrementingThread, NULL);
+    
+    printf("The magic number is: %d\n", i);
+    return 0;
+}
