3eab5fa57734412bc5735d463306c42f93f46e6c
mcclauba <mcclauba@gmail.com>
2019-01-16 11:29:03 +0100

    ex2 start

--- ex1/part4/c/foo.c
+++ ex1/part4/c/foo.c
@@ -5,14 +5,14 @@ int i = 0;
 
 // Note the return type: void*
 void* incrementingThreadFunction(){
-    for (int j = 0; j < 100000; j++){
+    for (int j = 0; j < 1000000; j++){
     	i++;
     }
     return NULL;
 }
 
 void* decrementingThreadFunction(){
-    for (int j = 0; j < 100000; j++){
+    for (int j = 0; j < 1000000; j++){
     	i--;
     }
     return NULL;
