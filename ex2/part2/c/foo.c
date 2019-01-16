c29e95f93fb86e06fdd5a60894f20bd619caaa01
Lars Sandberg <larslsa@stud.ntnu.no>
2019-01-16 12:33:07 +0100

    Create foo.c

---
+++ ex2/part2/c/foo.c
@@ -0,0 +1,35 @@
+#include <pthread.h>
+#include <stdio.h>
+
+int i = 0;
+
+// Note the return type: void*
+void* incrementingThreadFunction(){
+    for (int j = 0; j < 1000000; j++) {
+	// TODO: sync access to i
+	i++;
+    }
+    return NULL;
+}
+
+void* decrementingThreadFunction(){
+    for (int j = 0; j < 1000000; j++) {
+	// TODO: sync access to i
+	i--;
+    }
+    return NULL;
+}
+
+
+int main(){
+    pthread_t incrementingThread, decrementingThread;
+    
+    pthread_create(&incrementingThread, NULL, incrementingThreadFunction, NULL);
+    pthread_create(&decrementingThread, NULL, decrementingThreadFunction, NULL);
+    
+    pthread_join(incrementingThread, NULL);
+    pthread_join(decrementingThread, NULL);
+    
+    printf("The magic number is: %d\n", i);
+    return 0;
+}
