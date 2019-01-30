28c640b5b442b0f074b55d47312e34c4b4a1e411
Sandbergo <larslsa@stud.ntnu.no>
2019-01-30 13:09:50 +0100

    ex3 approved

--- ex3/udp.exs
+++ ex3/udp.exs
@@ -1,8 +1,6 @@
 # Group 25
 
 
-
-
 defmodule Receiver do
   require Logger
 
--- ex3/udp_rec.py
+++ ex3/udp_rec.py
@@ -1,12 +1,12 @@
 import socket
     
-UDP_IP = "10.100.23.134" # 172.18.0.1
+
+UDP_IP = "" 
 UDP_PORT = 30000
    
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,0)
 sock.bind((UDP_IP, UDP_PORT))
    
 while True:
-    print("sjur")
     data, addr = sock.recvfrom(1024)
     print("received message:", data)
