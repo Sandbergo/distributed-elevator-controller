51b7a25b5f280401d2abc43c76a4210125a89de8
student16 <stud@stud.ntnu.no>
2019-02-06 15:17:04 +0100

    day 4 end

--- ex4/net_mod.exs
+++ ex4/net_mod.exs
@@ -1,7 +1,15 @@
-# UDP, elixir Node, JSON, peer-to-peer
-
-
 defmodule NetworkModule do
+    @modeludoc """
+    Peer-to-peer communication using elixir Node
+    
+        1. ssh into remote 
+        2. ies --name nodename@10.100.23.000 local and remote
+        2. Node.set_cookie :yeet local and remote
+        3. Node.ping :"nodename@10.100.23.000"
+        4. PID = Node.spawn_link :"nodename@10.100.23.000", fn -> NetworkModule ?????
+        5. send pid, {string} ???
+    
+    """
 
     def init(queue) do
         {:ok, queue}
@@ -19,25 +27,51 @@ defmodule NetworkModule do
 
     
     def receive() do
-        IO.puts "received"
+        receive do
+            {string} ->        # something sent matches this datatype 
+                IO.puts string
+            end
+        IO.puts "received something else"
+
     end
     
     def main() do
         IO.puts "Alive: ", Node.alive?(), "; List: ", Node.list() 
+
+        receive()
     end
     
-    get_IP() do
-	{ok, Network_interfaces} = inet:getifaddrs(),
-	case proplists:get_value("eno1", Network_interfaces, undefined) do
-		undefined ->  # Non-Linux (personal computers)
-			{ok, Addresses} = inet:getif() 			    # Undocumented function returning all local IPs
-			inet_parse:ntoa(element(1, hd(Addresses)))  # Chooses the first IP and parses it to a string
 
-		Interface do  % Linux (at realtime lab computers)
-			IP_address = proplists:get_value(addr, Interface)
-			inet_parse:ntoa(IP_address)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    """
+    get_IP() do
+        {ok, Network_interfaces} = inet:getifaddrs(),
+        case proplists:get_value("eno1", Network_interfaces, undefined) do
+            undefined ->  # Non-Linux (personal computers)
+                {ok, Addresses} = inet:getif() 			    # Undocumented function returning all local IPs
+                inet_parse:ntoa(element(1, hd(Addresses)))  # Chooses the first IP and parses it to a string
+
+            Interface do  % Linux (at realtime lab computers)
+                IP_address = proplists:get_value(addr, Interface)
+                inet_parse:ntoa(IP_address)
 	end
-    
+    """
 
 
 end
