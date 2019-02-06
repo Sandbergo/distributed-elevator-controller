9d3313e59bc181c9538c9856da5f587114011cb4
student16 <stud@stud.ntnu.no>
2019-02-06 12:43:29 +0100

    day 4

--- ex4/net_mod.exs
+++ ex4/net_mod.exs
@@ -1,7 +1,29 @@
-# UDP, elixir Node, JSON
+# UDP, elixir Node, JSON, peer-to-peer
 
 
 defmodule NetworkModule do
-    use GenServer
 
+    def init(queue) do
+        {:ok, queue}
+    end
+
+
+    def start_link() do
+        GenServer.start_link(__MODULE__, :queue.new())
+    end
+
+
+    def send() do
+
+    end
+
+    
+    def receive() do
+        IO.puts "received"
+    end
+    
+    def main() do
+        IO.puts "Alive: ", Node.alive?(), "; List: ", Node.list() 
+    end
+    
 end
