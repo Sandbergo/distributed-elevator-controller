e15ea5e3c4cbbbee1e0c1a2147940b83940de394
Sandbergo <larslsa@stud.ntnu.no>
2019-03-23 16:20:58 +0100

    make bash loop and fix hexdocs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule Elevator.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "Elevator",
+      docs: [
+        main: "README.md", # The main page in the docs
+        extras: ["README.md", "SPECIFICATION.md"]
+      ]
     ]
   end
 
@@ -23,7 +30,7 @@ defmodule Elevator.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      #{:ex_doc, "~> 0.19"}
+      {:ex_doc, "~> 0.19"}
     ]
   end
 end
